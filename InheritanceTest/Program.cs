ca68a8a925dc66fc9a639fda66451e02fead2691
Fabricio Dias <fabricio270909@gmail.com>
2024-12-29 16:48:05 -0300

    Upcasting

--- InheritanceTest/Program.cs
+++ InheritanceTest/Program.cs
@@ -11,5 +11,14 @@ class Program
         Console.WriteLine(bAccount.Balance);
         // bAccount.Balance = 100.00; dá erro por Balance ser protected e acessivel apenas pela subclasse
         bAccount.Deposit(100.00);
+        
+        Account account = new Account(1001, "Alex", 0.00);
+        BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 0.00, 500.00);
+        
+        // Upcasting
+        Account account1 = businessAccount; //Convertendo uma variável BusinessAccount em Account
+        Account account2 = new BusinessAccount(1003, "Bob", 0.00, 200.00); // Instancia direta
+        Account account3 = new SavingsAccount(1004, "Ana", 0.00, 0.01);
+        
     }
 }
