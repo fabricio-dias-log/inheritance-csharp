5f2d6bb50d7b0bc88409500154fe355d0ea032a5
Fabricio Dias <fabricio270909@gmail.com>
2024-12-29 16:26:52 -0300

    Tests with protected attributes

--- InheritanceTest/Program.cs
+++ InheritanceTest/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace InheritanceTest;
+﻿using InheritanceTest.Entities;
+
+namespace InheritanceTest;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        BusinessAccount bAccount = new BusinessAccount(8010, "Bob Brown", 100.00, 500.00);
+
+        Console.WriteLine(bAccount.Balance);
+        // bAccount.Balance = 100.00; dá erro por Balance ser protected e acessivel apenas pela subclasse
+        bAccount.Deposit(100.00);
     }
 }
